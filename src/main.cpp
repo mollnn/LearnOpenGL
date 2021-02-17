7a5a389a7534f28f5204d8c24a2081739b3d2462
mollnn <1084918073@qq.com>
2021-02-17 15:12:55 +0800

    key callback

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+
 int main()
 {
 	// glfw: initialize and configure
@@ -12,9 +14,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //uncomment this statement to fix compilation on OS X
-#endif
+
 	// glfw window creation
 	GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
@@ -24,22 +24,39 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+
 	// glad: load all OpenGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
 	// render loop
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.0f, 1.f, 0.0f, 1.0f);
+		glfwPollEvents();
+
+		// render process here
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
 		// glfw: swap buffers and poll IO events (keyspressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
 	// glfw: terminate, clearing all previously allocated GLFWresources.
 	glfwTerminate();
 	return 0;
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	// 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
+	// 关闭应用程序
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
