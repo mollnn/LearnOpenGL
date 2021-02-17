b315f6a66af5f60abe2aae083c5b3d71890b40bb
mollnn <1084918073@qq.com>
2021-02-17 17:53:16 +0800

    hello triangle: ex2

--- src/main.cpp
+++ src/main.cpp
@@ -123,40 +123,51 @@ void render()
 	GLfloat vertices[] = {
 		-0.5, 0, 0,
 		-0.25, 0.5, 0,
+		0, 0, 0};
+	GLfloat vertices2[] = {
 		0, 0, 0,
 		0.25, 0.5, 0,
 		0.5, 0, 0};
 	GLuint indices[] = {
 		// Note that we start from 0!
-		0, 1, 2,
-		2, 3, 4};
+		0, 1, 2};
 
 	// **********************************************
-	// 顶点数组与缓冲对象的绑定
-	GLuint VBO, VAO, EBO; // 缓冲对象的编号
-	// 生成顶点数组与缓冲对象
-	glGenVertexArrays(1, &VAO); // 顶点数组
-	glGenBuffers(1, &VBO);		// 顶点缓冲
-	glGenBuffers(1, &EBO);		// 索引缓冲
+	// 顶点数组与缓冲对象
+	GLuint VBO1, VAO1, EBO;		 // 缓冲对象的编号
+	GLuint VBO2, VAO2;			 // 缓冲对象的编号
+	glGenVertexArrays(1, &VAO1); // 顶点数组
+	glGenBuffers(1, &VBO1);		 // 顶点缓冲
+	glGenVertexArrays(1, &VAO2); // 顶点数组
+	glGenBuffers(1, &VBO2);		 // 顶点缓冲
+	glGenBuffers(1, &EBO);		 // 索引缓冲
 
-	glBindVertexArray(VAO);						// 绑定顶点数组
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);			// 绑定顶点缓冲
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // 绑定索引缓冲
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);		 // 复制顶点数组到一个顶点缓冲中
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // 复制我们的索引数组到一个索引缓冲中
-
-	// 设定顶点属性指针（下面的 0 是位置值）
+	glBindVertexArray(VAO1);														   // 绑定顶点数组
+	glBindBuffer(GL_ARRAY_BUFFER, VBO1);											   // 绑定顶点缓冲
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);										   // 绑定索引缓冲
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);		   // 复制顶点数组到一个顶点缓冲中
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);   // 复制我们的索引数组到一个索引缓冲中
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0); // 该如何解析顶点数据
 	glEnableVertexAttribArray(0);													   // 激活
+	glBindBuffer(GL_ARRAY_BUFFER, 0);												   // 解绑 VBO
+	glBindVertexArray(0);															   // 解绑 VAO
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0); // 解绑 VBO
-	glBindVertexArray(0);			  // 解绑 VAO
+	glBindVertexArray(VAO2);														   // 绑定顶点数组
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);											   // 绑定顶点缓冲
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);										   // 绑定索引缓冲
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);	   // 复制顶点数组到一个顶点缓冲中
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);   // 复制我们的索引数组到一个索引缓冲中
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0); // 该如何解析顶点数据
+	glEnableVertexAttribArray(0);													   // 激活
+	glBindBuffer(GL_ARRAY_BUFFER, 0);												   // 解绑 VBO
+	glBindVertexArray(0);															   // 解绑 VAO
 
 	// --分割线-- 以下代码必须放在消息循环中
-
 	glUseProgram(shaderProgram);						 // 激活着色器程序
-	glBindVertexArray(VAO);								 // 绑定 VA0
+	glBindVertexArray(VAO1);							 // 绑定 VA0
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // 绘制
+	glBindVertexArray(0);								 // 解绑 VAO
+	glBindVertexArray(VAO2);							 // 绑定 VA0
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // 绘制
 	glBindVertexArray(0);								 // 解绑 VAO
 }
